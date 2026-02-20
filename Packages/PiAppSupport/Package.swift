1328152173bfe0d737f6538cc66faf1312a7d7ba
Jonathan Wight <jwight@mac.com>
2026-02-20 08:53:50 -0800

    Add PiSwift package dependency to PiAppSupport

--- Packages/PiAppSupport/Package.swift
+++ Packages/PiAppSupport/Package.swift
@@ -14,6 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
+        .package(url: "https://github.com/schwa/PiSwift", branch: "main"),
         .package(url: "https://github.com/migueldeicaza/SwiftTerm", branch: "main"),
     ],
     targets: [
