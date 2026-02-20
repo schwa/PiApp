f8e21b4248f34138d73fa873d0b34f3ea538bbb1
Jonathan Wight <jwight@mac.com>
2026-02-20 08:37:39 -0800

    Initial Commit

---
+++ PiApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PiApp
+//
+//  Created by Jonathan Wight on 2/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PiApp/PiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiAppApp.swift
+//  PiApp
+//
+//  Created by Jonathan Wight on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct PiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
