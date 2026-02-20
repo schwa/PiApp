eb5d1191fdb6f57ff4de9cbf17255f45717e7466
Jonathan Wight <jwight@mac.com>
2026-02-20 12:45:27 -0800

    Switch to schwa/PiSwift remote dependency

--- Packages/PiAppSupport/Package.swift
+++ Packages/PiAppSupport/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(path: "/Users/schwa/Projects/PiSwift"),
+        .package(url: "https://github.com/schwa/PiSwift", branch: "main"),
         .package(url: "https://github.com/migueldeicaza/SwiftTerm", branch: "main"),
     ],
     targets: [
